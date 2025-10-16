bbdec8c791f6c7938124f2963e84349ef4e978e1
Vladislavfvv <96388327+Vladislavfvv@users.noreply.github.com>
2025-10-16 18:24:50 +0300

    Change afterPropertySet method

--- src/com/myspring/service/FlyingService.java
+++ src/com/myspring/service/FlyingService.java
@@ -2,17 +2,26 @@ package com.myspring.service;
 
 import com.myspring.annotation.Autowired;
 import com.myspring.annotation.Component;
+import com.myspring.configuration.DisposableBean;
 import com.myspring.configuration.InitializingBean;
 import com.myspring.entity.Plane;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 @Component
-public class FlyingService  implements InitializingBean {
+public class FlyingService implements InitializingBean, DisposableBean {
+    @Override
+    public void destroy() {
+        System.out.println("FlyingService очищен через DisposableBean");
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
-        System.out.println("FlyingService bean init");
+        System.out.println("Method afterPropertiesSet() in " + MethodHandles.lookup().lookupClass().getSimpleName() + " was called");
     }
+
+
 
     @Autowired
     private Plane planes;
